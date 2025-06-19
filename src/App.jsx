d8c2aa7b4574da1b2a40609649773993ebcaa0c6
vestige <vestige@users.noreply.github.com>
2025-06-19 23:37:18 +0900

    Add Event Page & Edit Event

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
+import CreateEvent from "./pages/CreateEvent";
+import EditEvent from "./pages/EditEvent";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/create-event" element={<CreateEvent />} />
+        <Route path="/event/:eventId/edit" element={<EditEvent />} />
       </Routes>
     </Router>
   );
