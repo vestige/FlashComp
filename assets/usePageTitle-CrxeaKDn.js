4412b2a2d9e091b0f6f232d725a81714bdd77ab8
vestige <vestige@users.noreply.github.com>
2026-03-07 12:31:54 +0000

    deploy: 1fbedf7b004922158a50759d19138d5b339e38ba

---
+++ assets/usePageTitle-CrxeaKDn.js
@@ -0,0 +1,1 @@
+import{r as s}from"./vendor-react-run9ZIVx.js";const o="FLASH FLASH コンペ";function e(t){s.useEffect(()=>{const r=t?`${t} | ${o}`:o;document.title=r},[t])}export{e as u};
--- assets/usePageTitle-oe2wavoe.js
+++
@@ -1,1 +0,0 @@
-import{r as s}from"./vendor-react-0yB3rn5Y.js";const o="FLASH FLASH コンペ";function e(t){s.useEffect(()=>{const r=t?`${t} | ${o}`:o;document.title=r},[t])}export{e as u};
